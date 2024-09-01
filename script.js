fdb6a81e8ffaf917af9660862454b22ca321a026
aslamaslam143 <aslamaslama503@gmail.com>
2024-09-01 16:42:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const b = document.querySelector('button.btn-no')
+b.addEventListener("mouseover",moveHover);
+
+
+let popup = document.getElementById("popup");
+
+function openPopup() {
+    popup.classList.add("open");
+}
+
+function closePopup() {
+    popup.classList.remove("open");
+}
+
+
+
+function moveHover(){
+    const i = Math.floor(Math.random()*350)+1;
+    const j = Math.floor(Math.random()*350)+1;
+
+
+    b.style.left = i+"px";
+    b.style.top = j+"px";
+
+}
